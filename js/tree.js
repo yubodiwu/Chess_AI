d625b4e0704e4e4372080c2f145dc81e9f23ae54
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-01-03 13:03:56 -0800

    tree constructor

---
+++ js/tree.js
@@ -0,0 +1,13 @@
+"use strict";
+class node (){
+    constructor(val){
+        this.val = val;
+        this.children = [];
+    }
+}
+
+class Tree (){
+    constructor(){
+        this.root = null;
+    }
+}
