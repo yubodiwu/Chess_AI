61824ab1f1417988392d941360ba5c35f4629a1b
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-01-03 11:34:28 -0800

    updated

--- js/index.js
+++ js/index.js
@@ -1,24 +1,17 @@
 var board,
-  game = new Chess();
+  game = new Chess(),
+  statusEl = $('#status'),
+  fenEl = $('#fen'),
+  pgnEl = $('#pgn');
 
 // do not pick up pieces if the game is over
-// only pick up pieces for White
+// only pick up pieces for the side to move
 var onDragStart = function(source, piece, position, orientation) {
-  if (game.in_checkmate() === true || game.in_draw() === true ||
-    piece.search(/^b/) !== -1) {
+  if (game.game_over() === true ||
+      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
     return false;
   }
-};
-
-var makeRandomMove = function() {
-  var possibleMoves = game.moves();
-
-  // game over
-  if (possibleMoves.length === 0) return;
-
-  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-  game.move(possibleMoves[randomIndex]);
-  board.position(game.fen());
 };
 
 var onDrop = function(source, target) {
@@ -32,14 +25,46 @@ var onDrop = function(source, target) {
   // illegal move
   if (move === null) return 'snapback';
 
-  // make random legal move for black
-  window.setTimeout(makeRandomMove, 250);
+  updateStatus();
 };
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 var onSnapEnd = function() {
   board.position(game.fen());
+};
+
+var updateStatus = function() {
+  var status = '';
+
+  var moveColor = 'White';
+  if (game.turn() === 'b') {
+    moveColor = 'Black';
+  }
+
+  // checkmate?
+  if (game.in_checkmate() === true) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
+  }
+
+  // draw?
+  else if (game.in_draw() === true) {
+    status = 'Game over, drawn position';
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move';
+
+    // check?
+    if (game.in_check() === true) {
+      status += ', ' + moveColor + ' is in check';
+    }
+  }
+
+  statusEl.html(status);
+  fenEl.html(game.fen());
+  pgnEl.html(game.pgn());
 };
 
 var cfg = {
@@ -50,3 +75,5 @@ var cfg = {
   onSnapEnd: onSnapEnd
 };
 board = ChessBoard('board', cfg);
+
+updateStatus();
