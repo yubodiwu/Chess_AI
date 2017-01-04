61583c7c61981d5cb949a4013b3935d133ed5bac
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-01-04 10:33:38 -0800

    valued middle of board

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -21,12 +21,48 @@ function getBoardValues(board) {
                 blackScore += pieceValues[letter];
             }
         }
+
+        if (i === 3 || i === 4) {
+            var [whiteMid, blackMid] = isMiddle(fen[i]);
+            whiteScore += whiteMid;
+            blackScore += blackMid;
+        }
     }
 
     return {
         whiteScore,
         blackScore
     };
+}
+
+function isMiddle(row) {
+    var ind = 0;
+    var white = 0;
+    var black = 0;
+
+    for (let char of row) {
+        if (!isNaN(char)) {
+            ind += Number(char);
+        } else {
+            if (possibleBlacks.indexOf(char) !== -1) {
+                if (ind === 3 || ind === 4) {
+                    black += 1;
+                } //else if (ind === 2 || ind === 5) {
+                //     black += 0.5;
+                // }
+            } else if (possibleWhites.indexOf(char) !== -1) {
+                if (ind === 3 || ind === 4) {
+                    white += 1;
+                } // else if (ind === 2 || ind === 5) {
+                    // white += 0.5;
+                // }
+            }
+
+            ind++;
+        }
+    }
+
+    return [white, black]
 }
 
 function createChildren(node) {
