842bd8820910405acbfeababae30625a8adb29ab
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-01-04 13:29:16 -0800

    cented board

--- js/AIMove.js
+++ js/AIMove.js
@@ -1,4 +1,4 @@
-'use strict';
+  'use strict';
 // jshint esversion: 6
 // jshint devel: true
 // jshint node: true
@@ -6,15 +6,12 @@
 // jshint mocha: true
 
 function AImove() {
-    var t0 = performance.now();
     var AIColor = 'black';
     var startBoard = tree.root;
-    var min = 999;
-    var max = -999;
     createChildren(startBoard);
 
     var futureBoardValues = startBoard.children.map(function(ele) {
-        return findBestMoveMaxi(ele, 1, max, min);
+        return findBestMoveMaxi(ele, 1)
     });
 
     var bestBoards = startBoard.children.reduce(function(accum, cur) {
@@ -29,32 +26,13 @@ function AImove() {
 
     var minVal = Math.min(...futureBoardValues);
     var minInds = futureBoardValues.reduce(function(accum, cur, i) {
-        if (cur === minVal) accum.push(i);
+        if (cur === minVal) {
+            accum.push(i);
+        }
 
         return accum;
     }, [])
-
     var minInd = minInds[Math.floor(Math.random() * minInds.length)];
     game.move(startBoard.children[minInd].prevMove)
     board.position(game.fen());
-    console.log(`time to move is ${performance.now() - t0}`);
-}
-
-function createChildren(node) {
-    var possibleMoves = node.board.moves();
-
-    if (possibleMoves.length === 0)
-        return;
-
-    node.children = []
-    // value of each moves
-    for (var i = 0; i < possibleMoves.length; i++) {
-        var possibleBoard = new Chess(node.board.fen());
-        possibleBoard.move(possibleMoves[i]);
-
-        let boardVals = getBoardValues(possibleBoard)
-
-        node.children.push(new Node(possibleMoves[i], possibleBoard, boardVals.whiteScore, boardVals.blackScore))
-    }
-
 }
